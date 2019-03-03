bdf22136ee860c80845b908150db7b69c87d09fc
Haram Kim <kimharam@gmail.com>
2019-03-03 15:35:39 -0500

    added webcam component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenshot: null,
+      tab: 0
+    };
+  }
+  
+  handleClick = () => {
+    const screenshot = this.webcam.getScreenshot();
+    this.setState({ screenshot });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Webcam
+              audio={false}
+              ref={node => this.webcam = node}
+            />
+        <div>
+          <h2>Screenshots</h2>
+          <div className='screenshots'>
+            <div className='controls'>
+              <button onClick={this.handleClick}>capture</button>
+            </div>
+            {this.state.screenshot ? <img src={this.state.screenshot} /> : null}
+          </div>
+        </div>
       </div>
     );
   }
