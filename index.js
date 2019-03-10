2a9c73adb98385f702359622c55ebe8bfe05899b
Ahad Rizvi <ar727@cornell.edu>
2019-03-10 16:37:20 -0400

    update

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const url = require("url");
 const proxy = require("express-http-proxy");
 const path = require("path");
 const express = require("express");
+const passport = require("passport");
+const Strategy = require("passport-github").Strategy;
+
+passport.use(new Strategy({
+    clientID: process.env["GITHUB_ID"],
+    clientSecret: process.env["GITHUB_SECRET"],
+    callbackURL: '/auth_callback'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+  }
+))
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+})
+
+passport.deserializeUser(function(object, cb) {
+  cb(null, object);
+})
 
 const app = express();
 
