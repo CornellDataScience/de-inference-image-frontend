43751247c36b9cbc32048aa41bfff563ebacfa08
Haram Kim <kimharam@gmail.com>
2019-03-10 17:00:49 -0400

    broken frontend

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    let boundOnReceive = (function (event) {
-      // this.setState({ screenshot: event.data }, function () { console.log(this.state.screenshot.slice(0, 50)); } );
-      this.setState({ screenshot: event.data });
-    }).bind(this);
-
     this.state = {
-      screenshot: null,
+      displayedImage: null,
       tab: 0,
-      socket: new WebSocket(makeWebsocketURL())
+      socket: null,
     };
-
-    this.state.socket.onmessage = boundOnReceive;
   }
   
   sendImage = () => {
-    let screenshot = this.webcam.getScreenshot();
-    this.state.socket.send(screenshot);
-    // this.setState({ screenshot: screenshot });
+    // handle null socket
+    if(this.state.socket) {
+      let screenshot = this.webcam.getScreenshot();
+
+      // send image to backend
+      this.state.socket.send(screenshot);
+
+      // draw image on canvas
+      this.setState({ displayedImage: screenshot });
+    }
   }
 
   render() {
@@ -40,7 +40,13 @@ class App extends Component {
             <div className='controls'>
               <button onClick={this.sendImage}>capture</button>
             </div>
-            {this.state.screenshot ? <img src={this.state.screenshot} /> : null}
+
+            <div style={{width:"500", height:"500", margin:"20px 60px", border:"1px solid blue"}}>
+              <div style={{width:"100%", height:"100%", position:"relative"}}>
+                {this.state.displayedImage ? <img id="displayedImage" src={this.state.displayedImage} style={{ position:"absolute", top:"0px", left:"0px"}} /> : null}
+                <canvas id="overlayCanvas" style={{ width:"500", height:"500", position:"absolute", top:"0px", left:"0px", "backgroundColor": "rgba(255,0,0,.1)"}}></canvas>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -48,14 +54,34 @@ class App extends Component {
   }
 
   componentDidMount() {
-      this.intervalID = setInterval( () => this.sendImage(), 500
-    );
+    // let socket = new WebSocket(makeWebsocketURL());
+    let socket = new WebSocket("ws://localhost:8765");
+    this.setState({ socket: socket }, (() => this.state.socket.onmessage = boundOnReceive).bind(this));
   }
 
   componentWillUnmount() {
-    clearInterval(this.intervalID);
+    
   }
 }
+
+let boundOnReceive = (function (event) {
+  // TODO: draw box
+  console.log(event.data)
+  if (event.data.name) {
+    let points = event.data["coordinates"];
+
+    let canvas = document.getElementById("overlayCanvas");
+    let context = canvas.getContext('2d');
+    context.beginPath();
+    // context.rect(188, 50, 200, 100);
+    context.rect(points[0], points[1], points[2], points[3]);
+    context.lineWidth = 20;
+    context.strokeStyle = 'red';
+    context.stroke();
+
+    // this.setState({ screenshot: event.data });
+  }
+}).bind(this);
 
 let makeWebsocketURL = function() {
   let loc = window.location, new_uri;
@@ -65,7 +91,9 @@ let makeWebsocketURL = function() {
       new_uri = "ws:";
   }
   new_uri += "//" + loc.host;
-  new_uri += loc.pathname + "/stream";
+  new_uri += loc.pathname + "stream";
+
+  // console.log(new_uri);
 
   return new_uri;
 }
