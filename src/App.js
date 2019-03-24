6622caf5c9c991602323eb1fc93d721b7d21f9b4
Aliyah Geer <amg369@cornell.edu>
2019-03-24 15:34:54 -0400

    Added Latency

--- src/App.js
+++ src/App.js
@@ -11,28 +11,33 @@ class App extends Component {
       currentImage: null,
       tab: 0,
       socket: null,
+      currentLatency: 0,
+      currentImageStartTime: null,
     };
 
     // bind methods
     this.boundOnReceive = this.boundOnReceive.bind(this);
     this.sendImage = this.sendImage.bind(this);
   }
+
   render() {
     return (
-      <div className="App"> 
+      <div className="App">
         <Webcam
           id="cameraView"
           audio={false}
           ref={node => this.webcam = node}
         />
+
+
         <div>
-          <h2>Screenshots</h2>
+        <div> currentLatency: {this.state.currentLatency} </div>
+      <h2>Screenshots</h2>
           <div className='screenshots'>
             <div className='controls'>
               <button onClick={this.sendImage}>capture</button>
             </div>
-
-            <canvas id="displayCanvas" width="640" height="480" /> 
+            <canvas id="displayCanvas" width="640" height="480" />
             {/* <div style={{width:"500", height:"500", margin:"20px 60px", border:"1px solid blue"}}>
               <div style={{width:"100%", height:"100%", position:"relative"}}>
                 {this.state.displayedImage ? <img id="displayedImage" src={this.state.displayedImage} style={{ position:"absolute", top:"0px", left:"0px"}} /> : null}
@@ -47,72 +52,95 @@ class App extends Component {
 
   componentDidMount() {
     let socket = new WebSocket(makeWebsocketURL());
-    // let socket = new WebSocket("ws://localhost:8765");
+    //let socket = new WebSocket("ws://localhost:8765");
     socket.onmessage = this.boundOnReceive;
     this.setState({ socket: socket });
+     //socket.onerror = this.boundOnReceive;
+     //socket.onclose = this.boundOnReceive;
+       //console.log("Socket is closed now.");
 
-    //TODO: start image sending process 
+
   }
+// // for setting latency
+//   setInterval() {
+//     startTime = Date.now()
+//     socket.emit('ping');
+//   }, 2000);
+//
+//   socket.on('pong', function(){
+//     currentLatency = Date.now() - startTime;
+//   }
+//
+    //TODO: start image sending process
+
 
   componentWillUnmount() {
-    
+
   }
 
   boundOnReceive(event) {
+    // updating currentlatency according to currentImageStartTime
+    const currentTime = new Date();
+    const currentLatency = this.state.currentImageStartTime - currentTime;
+
+    this.setState({
+      currentLatency: currentLatency,
+    })
     // extract event data
     let faceData =  JSON.parse(event.data);
-    
+
     // get canvas
     let canvas = document.getElementById("displayCanvas");
     let context = canvas.getContext('2d');
-  
+
     // draw screenshot
     let ssImg = new Image();
     ssImg.onload = function() {
       // add image
       context.drawImage(ssImg, 0, 0, 640, 480);
 
-      // draw boxes 
+      // draw boxes
       if (event.data) {
         for(let i = 0; i < faceData.length; i++){
           let points = faceData[i]["coordinates"];
           // draw image
           context.beginPath();
-          context.rect(points[3], points[2], points[2] - points[0], points[3] - points[1]); 
+          context.rect(points[3], points[2], points[2] - points[0], points[3] - points[1]);
           context.lineWidth = 1.5;
           context.strokeStyle = 'red';
           context.stroke();
-    
+
           // add label
           context.fontSize = "10px";
           context.fillStyle = "red";
-          context.fillText(faceData[i]["name"], points[3] + 5, points[2] - 5); 
+          context.fillText(faceData[i]["name"], points[3] + 5, points[2] - 5);
         }
       }
     };
     ssImg.src = this.state.currentImage;
-    
+
     // send new image immediately
     this.sendImage();
 
     // after we draw, send a new image in 0.1s
     // setTimeout(function() { this.sendImage(); }.bind(this), 100);
   }
-  
+
   sendImage() {
     // handle null socket
     if(this.state.socket) {
       let screenshot = this.webcam.getScreenshot();
-  
+
       // store current image
-      this.setState({ currentImage: screenshot }, (
+      this.setState({ currentImage: screenshot },
         // send image to backend in callback after state is updated
-        function () { 
+        () => {
+          this.setState({currentImageStartTime: new Date()});
           this.state.socket.send(screenshot);
-          console.log("sent"); 
-        }).bind(this) 
-      );
-      
+          console.log("sent");
+        })
+
+
     }
   }
 }
@@ -124,7 +152,7 @@ let makeWebsocketURL = function() {
   } else {
       new_uri = "ws:";
   }
-  
+
   let new_host = loc.host.slice(0, loc.host.indexOf(':'));
   new_uri += "//" + new_host + ":8080";
   new_uri += loc.pathname;
