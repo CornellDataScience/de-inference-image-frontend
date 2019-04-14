90da2277f7ecf0402b29c997d58630e9e05b5bc5
Haram Kim <kimharam@gmail.com>
2019-04-14 16:00:33 -0400

    maybe fixed issue with websocket connection

--- src/App.js
+++ src/App.js
@@ -152,8 +152,12 @@ let makeWebsocketURL = function() {
       new_uri = "ws:";
   }
 
-  let new_host = loc.host.slice(0, loc.host.indexOf(':'));
-  new_uri += "//" + new_host + ":8080";
+  // for when running locally
+//   let new_host = loc.host.slice(0, loc.host.indexOf(':'));
+//   new_uri += "//" + new_host + ":8080";
+//   new_uri += loc.pathname;
+
+  new_uri += "//" + loc.host;
   new_uri += loc.pathname;
 
   return new_uri;
